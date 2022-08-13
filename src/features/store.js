20d524756c85b3e8f4708e3d7c6e531962b7b404
Msotafa Mohamed <60571462+Mostafashadow1@users.noreply.github.com>
2022-08-13 13:55:16 +0200

    Delete src directory

--- src/features/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { curenciesApi } from "services/currenciesApi";
-import { newsApi } from "services/newsApi";
-export default configureStore({
-  reducer: {
-    [curenciesApi.reducerPath]: curenciesApi.reducer,
-    [newsApi.reducerPath]: newsApi.reducer,
-  },
-});
