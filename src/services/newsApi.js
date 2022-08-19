adbb4d0d3daaaa025092ece4bce1707969715864
Msotafa Mohamed <60571462+Mostafashadow1@users.noreply.github.com>
2022-08-19 12:58:54 +0200

    Update newsApi.js

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // base URL
-const baseUrl = process.env.REACT_APP_NEWS_BASEURL;
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
 // API Headers
 const newsApiHeaders = {
   "X-BingApis-SDK": "true",
   "X-RapidAPI-Key": process.env.REACT_APP_NEWS_KEY,
-  "X-RapidAPI-Host": process.env.REACT_APP_NEWS_HOST,
+  "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
 };
 // helper function make requeset
 const makeRequest = (url) => ({ url, headers: newsApiHeaders });
